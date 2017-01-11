10e30307dfdba70924eea57a49ae9984cd652945
JeffMinsungKim <ziznzkak@gmail.com>
2017-01-11 16:31:43 +0900

    Updated.

---
+++ src/test/scala/tutorial/Operations/OperationTutorialTest.scala
@@ -0,0 +1,65 @@
+package tutorial.Operations
+
+import org.junit.Test
+import org.scalatest.junit.JUnit3Suite
+
+/**
+  * Created by JeffMinsungKim on 11/01/2017.
+  */
+class OperationTutorialTest extends JUnit3Suite{
+
+  @Test
+  def testScalaOperatorStyle(): Unit = {
+    val s = "Hello, World!"
+    val result = s indexOf 'o' // Scala invokes s.indexOf('o')
+    assert(result == 4)
+  }
+
+  @Test
+  def testLongTypeNumber(): Unit = {
+    val n = 2L * 3L
+    assert(n.isInstanceOf[Long])
+  }
+
+  @Test
+  def testFloatTypeNumber(): Unit = {
+    val n = 11.0f * 4.0f
+    assert(n.isInstanceOf[Float])
+  }
+
+  @Test
+  def testDoubleTypeNumber(): Unit = {
+    val n = 1.2 + 3.5
+    assert(n.isInstanceOf[Double])
+  }
+
+  @Test
+  def testNegativeNumber(): Unit = {
+    val n = 1 + -3
+    assert(n == -2)
+    assert(-n == 2)
+  }
+
+  @Test
+  def testPositiveNumber(): Unit = {
+    val n = +3
+    assert(n == 3)
+  }
+
+  @Test
+  def testLogicalOperation(): Unit = {
+    val untrue = !true
+    val isTrue = true
+    val case1 = isTrue || !isTrue
+    val case2 = isTrue && !isTrue
+    assert(!untrue)
+    assert(case1)
+    assert(!case2)
+  }
+
+  @Test
+  def testLeftShiftOperation(): Unit = {
+    val result = 2 + 2 << 2
+    assert(result == 16)
+  }
+}
---
+++ src/test/scala/tutorial/Operations/StringInterpolationTest.scala
@@ -0,0 +1,37 @@
+package tutorial.Operations
+
+import org.junit.Test
+import org.scalatest.junit.JUnit3Suite
+
+/**
+  * Created by JeffMinsungKim on 11/01/2017.
+  */
+class StringInterpolationTest extends JUnit3Suite {
+
+
+  // Scala provides two other string interpolators by default: raw and f.
+  // The raw string interpolator behaves like s, except it does not recognize
+  // character literal escape sequences.
+  // For example, the following statement prints four backslashes, not two.
+  // println(raw"No\\\\escape!") prints: No\\\\escape!
+  // In Scala, string interpolation is implemented by rewriting code at compile time.
+
+  @Test
+  def testStringInterpolation(): Unit = {
+    val name = "reader"
+    assert(s"Hello!, $name" == "Hello!, reader")
+  }
+
+  @Test
+  def testSumStringInterpolation(): Unit = {
+    assert(s"${6 * 7}".toInt == 42)
+  }
+
+  // The f string interpolator allows you to attach printf-style formatting
+  // instructions to embedded expressions.
+  @Test
+  def testFInterpolator(): Unit = {
+    val pi = "Pi"
+    print(f"$pi is approximately ${math.Pi}%.8f")
+  }
+}
